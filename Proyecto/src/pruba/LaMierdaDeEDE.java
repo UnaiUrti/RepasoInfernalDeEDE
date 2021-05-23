82ceb1da260d3ff45b45a455ace54ce7489d6f53
1dam <1dam@TartangaLH.eus>
2021-05-23 19:25:44 +0200

    Vamos a ver

---
+++ Proyecto/src/pruba/LaMierdaDeEDE.java
@@ -0,0 +1,12 @@
+package pruba;
+
+public class LaMierdaDeEDE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("A ver que coño pasa aquí");
+
+	}
+
+}
