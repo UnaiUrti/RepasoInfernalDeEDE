169bad71cc43b84cb8a6bf3b613bda7f0c96b78e
UnaiUrti <83644624+UnaiUrti@users.noreply.github.com>
2021-05-23 19:26:57 +0200

    Update LaMierdaDeEDE.java

--- Proyecto/src/pruba/LaMierdaDeEDE.java
+++ Proyecto/src/pruba/LaMierdaDeEDE.java
@@ -5,7 +5,8 @@ public class LaMierdaDeEDE {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("A ver que coño pasa aquí");
+		System.out.println("A ver que coÃ±o pasa aquÃ­");
+		System.out.println("Que somos imbÃ©ciles");
 
 	}
 
